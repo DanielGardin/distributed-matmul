88a23f4b504db6fd74ea48b6f36b69a18aea2bdc
Daniel Gardin <gardin.dan@gmail.com>
2025-11-20 14:58:56 -0300

    refactor: Re-organizing codebase and build system

---
+++ src/experiment.c
@@ -0,0 +1,298 @@
+// ====================================
+// Experiment configs and results
+// ====================================
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <math.h>
+
+typedef struct {
+    int size;                // global matrix dimension (N)
+    int block_size;          // block or tile size
+    int warmup_repeats;      // number of non-timed, warmup repetitions
+    int repeats;             // number of timed repetitions
+    int implementation_id;   // algorithm variant selector
+    int seed;                // RNG seed
+
+    int use_openblas;        // use OpenBLAS
+    int use_mpi;             // enable MPI distribution
+
+    int proc_rows;           // MPI process grid rows
+    int proc_cols;           // MPI process grid cols
+
+    int verify;              // compare result against serial baseline
+    char *path;              // path to save a .csv containing the benchmark results
+} ExperimentConfig;
+
+typedef struct {
+    double *times;
+    double *comm_times;
+    double *comp_times;
+    double overall_time;
+    int repeats;
+    int correct;
+} ExperimentResult;
+
+// Implementation names for user
+const char *impl_name(int id) {
+    switch (id) {
+        case 0: return "Serial naive";
+        case 1: return "Serial blocked";
+        case 2: return "Serial OpenBLAS";
+        case 3: return "MPI SUMMA";
+        case 4: return "MPI SUMMA+OpenBLAS";
+        default: return "Unknown implementation";
+    }
+}
+
+ExperimentResult create_results(const ExperimentConfig *cfg, int rank) {
+    double *times = NULL;
+    double *comm_times = NULL;
+    double *comp_times = NULL;
+
+    if (rank == 0) {
+        times = malloc(cfg->repeats * sizeof(double));
+        comm_times = calloc(cfg->repeats, sizeof(double));
+        comp_times = calloc(cfg->repeats, sizeof(double));
+    }
+
+    return (ExperimentResult){
+        .times = times,
+        .comm_times = comm_times,
+        .comp_times = comp_times,
+        .repeats = cfg->repeats,
+        .correct = 1
+    };
+}
+
+void log_timings(
+    ExperimentResult *result, int iter,
+    double total_time, double comm_time, double comp_time, int rank
+) {
+    if (rank != 0) return;
+
+    if (iter >= result->repeats)
+        fprintf(stderr, "WARNING: Trying to log more than %d repeatitions.\n", iter);
+    
+    result->times[iter] = total_time;
+    result->comm_times[iter] = comm_time;
+    result->comp_times[iter] = comp_time;
+}
+
+double mean(double *array, int size) {
+    double mean_val = 0.0;
+    for (int i = 0; i < size; i++)
+        mean_val += array[i];
+    
+    return mean_val / size;
+}
+
+double std(double *array, int size) {
+    double mean_val = mean(array, size);
+    double std_val = 0;
+
+    double time_res;
+    for (int i=0; i<size; i++) {
+        time_res = array[i] - mean_val;
+        std_val += time_res * time_res;
+    }
+
+    if (std_val > 0) {
+        std_val /= (size-1);
+        std_val = sqrt(std_val);
+    }
+
+    return std_val;
+}
+
+double max(double *array, int size) {
+    double max_val = array[0];
+    double val;
+
+    for (int i = 1; i < size; i++) {
+        val = array[i];
+
+        max_val = val > max_val ? val : max_val;
+    }
+
+    return max_val;
+}
+
+double min(double *array, int size) {
+    double min_val = array[0];
+    double val;
+
+    for (int i = 1; i < size; i++) {
+        val = array[i];
+
+        min_val = val < min_val ? val : min_val;
+    }
+
+    return min_val;
+}
+
+const char *bars[9] = {
+    "_",
+    "▁",
+    "▂",
+    "▃",
+    "▄",
+    "▅",
+    "▆",
+    "▇",
+    "█"
+};
+void print_histogram(
+    double *array, int size, int n_bins
+) {
+    double max_val = max(array, size);
+    double min_val = min(array, size);
+
+    double width = (max_val - min_val) / n_bins;
+    if (width == 0) width = 1;
+
+    int *bin_count = calloc(n_bins, sizeof(int));
+
+    for (int i = 0; i < size; i++) {
+        int idx = ((array[i] - min_val) / width);
+        if (idx >= n_bins) idx = n_bins - 1;
+
+        bin_count[idx]++;
+    }
+
+    int max_count = 0;
+    for (int i=0; i<n_bins; i++)
+        if (bin_count[i] > max_count) max_count = bin_count[i];
+
+    double freq;
+    int height;
+    for (int i = 0; i < n_bins; i++) {
+        freq = (double)bin_count[i] / max_count;
+        height = (int)(8 * freq);
+        printf("%s", bars[height]);
+    }
+    printf("\n");
+
+    free(bin_count);
+}
+
+static char *units[4] = {"s ", "ms", "μs", "ns"};
+struct Time { double time; char *unit; };
+struct Time process_time(double time) {
+    int idx = 0;
+    while (time < 1 && time > 0 && idx < 4) {
+        time *= 1000;
+        idx++;
+    }
+
+    return (struct Time){.time=time, .unit=units[idx]};
+}
+
+void print_results(const ExperimentConfig *cfg, const ExperimentResult *res) {
+    struct Time overall_time = process_time(res->overall_time);
+    struct Time mean_time = process_time(mean(res->times, res->repeats));
+
+    printf("================     Experiment Results     ================\n");
+    printf("%d samples evaluated in a total of %.3lf %s\n", cfg->repeats, overall_time.time, overall_time.unit);
+    printf("Matrix size:                        %d x %d\n", cfg->size, cfg->size);
+    printf("Block size:                         %d\n", cfg->block_size);
+    printf("Warmup rounds:                      %d\n", cfg->warmup_repeats);
+    printf("Repetitions:                        %d\n", cfg->repeats);
+    if (cfg->use_mpi)
+        printf("MPI grid:                           %d x %d\n", cfg->proc_rows, cfg->proc_cols);
+    printf("OpenBLAS:                           %s\n", cfg->use_openblas ? "ON" : "OFF");
+    printf(
+        "Implementation ID:                  %d (%s)\n",
+        cfg->implementation_id, impl_name(cfg->implementation_id)
+    );
+    if (cfg->verify)
+        printf("Verification:                       %s%s%s\n",
+            res->correct ? "\033[1;32mPASSED" : "\033[1;31mFAILED",
+            "\033[0m", "");
+    printf("\n");
+
+    struct Time std_time = process_time(std(res->times, res->repeats));
+    struct Time max_time = process_time(max(res->times, res->repeats));
+    struct Time min_time = process_time(min(res->times, res->repeats));
+
+    printf(
+        "Time  (mean ± σ):                   %6.3lf %s ± %6.3lf %s\n",
+        mean_time.time, mean_time.unit, std_time.time, std_time.unit
+    );
+    printf(
+        "Time  (min … max):                  %6.3lf %s … %6.3lf %s\n",
+        min_time.time, min_time.unit, max_time.time, max_time.unit
+    );
+
+    printf("\n      ");
+    print_histogram(res->times, res->repeats, 48);
+    printf(
+        "%3.1lf %s             frequency by time             %3.1lf %s\n", 
+        min_time.time, min_time.unit, max_time.time, max_time.unit
+    );
+    printf("\n");
+
+    
+    if (cfg->use_mpi) {
+        struct Time mean_comp_time = process_time(mean(res->comp_times, res->repeats));
+        struct Time std_comp_time = process_time(std(res->comp_times, res->repeats));
+        struct Time mean_comm_time = process_time(mean(res->comm_times, res->repeats));
+        struct Time std_comm_time = process_time(std(res->comm_times, res->repeats));
+
+        double *comp_perc = malloc(res->repeats * sizeof(double));
+        double *comm_perc = malloc(res->repeats * sizeof(double));
+
+        for (int i = 0; i < res->repeats; i++) {
+            comp_perc[i] = res->comp_times[i] / res->times[i];
+            comm_perc[i] = res->comm_times[i] / res->times[i];
+        }
+        double mean_comp_perc = mean(comp_perc, res->repeats);
+        double std_comp_perc = std(comp_perc, res->repeats);
+        double mean_comm_perc = mean(comm_perc, res->repeats);
+        double std_comm_perc = std(comm_perc, res->repeats);
+
+        printf(
+            "Comp. time (mean ± σ): %6.2lf %s ± %6.2lf %s  (%3.2lf ± %3.2lf)%%\n",
+            mean_comp_time.time, mean_comp_time.unit, std_comp_time.time, std_comp_time.unit,
+            mean_comp_perc, std_comp_perc
+        );
+        printf(
+            "Comm. time (mean ± σ): %6.2lf %s ± %6.2lf %s  (%3.2lf ± %3.2lf)%%\n",
+            mean_comm_time.time, mean_comm_time.unit, std_comm_time.time, std_comm_time.unit,
+            mean_comm_perc, std_comm_perc
+        );
+
+        free(comp_perc);
+        free(comm_perc);
+    }
+
+    printf("============================================================\n");
+}
+
+void export_csv(char *path, const ExperimentConfig *cfg, const ExperimentResult *res) {
+    FILE *fp = fopen(path, "w");
+
+    fprintf(fp, "rep_id, size, block_size, BLAS, MPI, grid, time");
+    if (cfg->use_mpi) {
+        fprintf(fp, ", comm_time, comp_time");
+    }
+    fputc('\n', fp);
+
+    for (int i = 0; i < res->repeats; i++) {
+        fprintf(
+            fp, "%d, %d, %d, %d, %d, %dx%d, %.9g",
+            i, cfg->size, cfg->block_size, cfg->use_openblas, cfg->use_mpi,
+            cfg->proc_rows, cfg->proc_cols, res->times[i]
+        );
+    
+        if (cfg->use_mpi) {
+            fprintf(
+                fp, ", %.9g, %.9g",
+                res->comm_times[i], res->comp_times[i]
+            );
+        }
+        fputc('\n', fp);
+    }
+
+    fclose(fp);
+}
--- src/main.c
+++ src/main.c
@@ -1,70 +1,62 @@
+// ====================================
+// Argument parser and main function
+// ====================================
+
 // Somthing related to unistd.h
 #define _POSIX_C_SOURCE 200809L
 
-#include <stdio.h>
-#include <stdlib.h>
-
 #include <unistd.h>
 #include <time.h>
-
-#include <math.h>
 
 #ifdef OPENMPI
 #include <mpi.h>
 #endif
 
 #define SERIAL_IMPLEMENTATIONS 3
+#define PARALLEL_IMPLEMENTATIONS 2
 
 #ifdef OPENMPI
-#define NUM_IMPLEMENTATIONS 5
 #define CLOCK_FN MPI_Wtime
+#define COMPILE_T 1
+#define BARRIER(comm) MPI_Barrier(comm)
 #else
-#define NUM_IMPLEMENTATIONS SERIAL_IMPLEMENTATIONS
 #define CLOCK_FN clock
+#define COMPILE_T 0
+#define BARRIER(comm) ((void)0)
+double g_comm = 0.0;
+double g_comp = 0.0;
 #endif
-
 
 #include "matrix.h"
 #include "matmul_interface.h"
 
-// ====================================
-// Experiment configs and results
-// ====================================
+#include "experiment.c"
 
-typedef struct {
-    int size;                // global matrix dimension (N)
-    int block_size;          // block or tile size
-    int warmup_repeats;      // number of non-timed, warmup repetitions
-    int repeats;             // number of timed repetitions
-    int implementation_id;   // algorithm variant selector
-    int seed;                // RNG seed
+ExperimentResult run_experiment(const ExperimentConfig *cfg, int rank);
 
-    int use_openblas;        // use OpenBLAS
-    int use_mpi;             // enable MPI distribution
+void print_help(const char *program_path) {
+    fprintf(stderr,
+        "Usage: %s [<path>] [options]\n\n"
+        "Positional arguments:\n"
+        "  <path>               CSV file to save the results (optional)\n\n"
+        "Options:\n"
+        "  -n <size>            Global matrix dimension (default: 512)\n"
+        "  -b <block_size>      Block or tile size (default: 64)\n"
+        "  -w <warmup_repeats>  Warm-up repetitions before timing (default: 0)\n"
+        "  -r <repeats>         Number of timed repetitions (default: 3)\n"
+        "  -i <id>              Implementation ID (see below)\n"
+        "  -s <seed>            Random seed (default: 0)\n"
+        "  -v                   Verify result against serial baseline\n"
+        "  -h                   Show this help message\n\n",
+        program_path
+    );
 
-    int proc_rows;           // MPI process grid rows
-    int proc_cols;           // MPI process grid cols
+    fprintf(stderr, "Implementation IDs:\n");
+    for (int i = 0; i < SERIAL_IMPLEMENTATIONS + COMPILE_T * PARALLEL_IMPLEMENTATIONS; i++)
+        fprintf(stderr, "  %2d: %s\n", i, impl_name(i));
+}
 
-    int verify;              // compare result against serial baseline
-    char *path;
-} ExperimentConfig;
-
-typedef struct {
-    double* times;
-    int repeats;
-    int correct;
-} ExperimentResult;
-
-void print_help(const char *program_path);
-ExperimentResult run_experiment(const ExperimentConfig* cfg, int rank);
-void print_results(const ExperimentConfig *cfg, const ExperimentResult *res);
-void export_csv(char *path, const ExperimentConfig *cfg, const ExperimentResult *res);
-
-// ====================================
-// Argument parser and main function
-// ====================================
-
-int parse_args(int argc, char **argv, ExperimentConfig *cfg) {
+int parse_args(int argc, char **argv, ExperimentConfig *cfg, int rank) {
     // Positional parameter
     if (argc > 1 && argv[1][0] != '-') {
         cfg->path = argv[1];
@@ -96,18 +88,6 @@ int parse_args(int argc, char **argv, ExperimentConfig *cfg) {
                 return 0;
             }
         }
-
-#ifndef OPENMPI
-    if (cfg->implementation_id >= SERIAL_IMPLEMENTATIONS) {
-        fprintf(
-            stderr,
-            "ERROR: implementation_id=%d requires MPI support. Please, compile with -DUSE_MPI=1.\n",
-            cfg->implementation_id
-        );
-        return 0;
-    }
-#endif
-
     if (cfg->block_size > cfg->size)
         cfg->block_size = cfg->size;
 
@@ -136,16 +116,57 @@ int parse_args(int argc, char **argv, ExperimentConfig *cfg) {
 #endif
 
         default:
-            fprintf(stderr, "ERROR: Unknown implementation_id=%d.\n", cfg->implementation_id);
-            print_help(argv[0]);
+            if (rank == 0) {
+                fprintf(
+                    stderr,
+                    (
+                        cfg->implementation_id > 0 &&
+                        cfg->implementation_id < SERIAL_IMPLEMENTATIONS + PARALLEL_IMPLEMENTATIONS
+                    ) ?
+                    "ERROR: implementation_id=%d requires MPI support. Please, compile with -DUSE_MPI=1.\n" :
+                    "ERROR: Unknown implementation_id=%d.\n",
+                    cfg->implementation_id
+                );
+                print_help(argv[0]);
+            }
             return 0;
     }
+
+#ifdef OPENMPI
+    int n_procs;
+    MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
+
+    if (cfg->use_mpi) {
+        int best_r = (int)ceil(sqrtf((float)n_procs));
+        while (n_procs % best_r) best_r++;
+
+        cfg->proc_rows = best_r;
+        cfg->proc_cols = n_procs / best_r;
+
+        if (rank == 0)
+            fprintf(
+                stderr, "INFO: Selected the best grid %d x %d\n", cfg->proc_rows, cfg->proc_cols
+            );
+
+    } else if (n_procs > 1 && rank == 0) {
+        fprintf(
+            stderr, "INFO: Running more than one process in a serial algorithm. "
+            "The results in every rank, besides the root, will be ignored.\n"
+        );
+    }
+#endif
 
     return 1;
 }
 
 int main(int argc, char** argv) {
+    double T0 = CLOCK_FN();
     int rank = 0;
+
+#ifdef OPENMPI
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+#endif
 
     ExperimentConfig cfg = {
         .size = 512,.block_size = 64, .warmup_repeats = 0,
@@ -153,78 +174,31 @@ int main(int argc, char** argv) {
         .use_openblas = 0, .proc_rows = 1, .proc_cols = 1, .verify = 0
     };
 
-    if (!parse_args(argc, argv, &cfg)) return 1;
-
-// MPI initiallization and grid selection
-#ifdef OPENMPI
-    MPI_Init(&argc, &argv);
-    int n_procs = 1;
-
-    MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-    if (cfg.use_mpi) {
-        int best_r = (int)sqrtf((float)n_procs);
-        while (n_procs % best_r) best_r++;
-
-        cfg.proc_rows = best_r;
-        cfg.proc_cols = n_procs / best_r;
-
-        if (rank == 0)
-            fprintf(
-                stderr, "INFO: Selected the best grid %d x %d\n", cfg.proc_rows, cfg.proc_cols
-            );
-    }
-#endif
+    if (!parse_args(argc, argv, &cfg, rank)) return 1;
 
     ExperimentResult results = run_experiment(&cfg, rank);
+
+    results.overall_time = (double)CLOCK_FN() - T0;
+
+#ifndef OPENMPI
+    results.overall_time /= CLOCKS_PER_SEC;
+#endif
 
     // Only print with the master process
     if (rank == 0) {
         print_results(&cfg, &results);
-        
+
         if (cfg.path) export_csv(cfg.path, &cfg, &results);
     }
     
     free(results.times);
+    free(results.comm_times);
+    free(results.comp_times);
 
     #ifdef OPENMPI
         MPI_Finalize();
     #endif
     return 0;
-}
-
-const char *impl_name(int id) {
-    switch (id) {
-        case 0: return "Serial naive";
-        case 1: return "Serial blocked";
-        case 2: return "Serial OpenBLAS";
-        case 3: return "MPI SUMMA";
-        case 4: return "MPI SUMMA+OpenBLAS";
-        default: return "Unknown implementation";
-    }
-}
-
-void print_help(const char *program_path) {
-    fprintf(stderr,
-        "Usage: %s [<path>] [options]\n\n"
-        "Positional arguments:\n"
-        "  <path>               CSV file to save the results (optional)\n\n"
-        "Options:\n"
-        "  -n <size>            Global matrix dimension (default: 512)\n"
-        "  -b <block_size>      Block or tile size (default: 64)\n"
-        "  -w <warmup_repeats>  Warm-up repetitions before timing (default: 0)\n"
-        "  -r <repeats>         Number of timed repetitions (default: 3)\n"
-        "  -i <id>              Implementation ID (see below)\n"
-        "  -s <seed>            Random seed (default: 0)\n"
-        "  -v                   Verify result against serial baseline\n"
-        "  -h                   Show this help message\n\n",
-        program_path
-    );
-
-    fprintf(stderr, "Implementation IDs:\n");
-    for (int i = 0; i < NUM_IMPLEMENTATIONS; i++)
-        fprintf(stderr, "  %2d: %s\n", i, impl_name(i));
 }
 
 // ====================================
@@ -267,7 +241,12 @@ int run_matmul(
     return 1;
 }
 
-ExperimentResult run_experiment(const ExperimentConfig* cfg, int rank) {
+inline void warmup_runs(Matrix *A, Matrix *B, Matrix *C, const ExperimentConfig *cfg) {
+    for (int r = 0; r < cfg->warmup_repeats; r ++)
+        run_matmul(A, B, C, cfg);
+}
+
+ExperimentResult run_experiment(const ExperimentConfig *cfg, int rank) {
     Matrix A = (Matrix){ .data = NULL, .rows = cfg->size, .cols = cfg->size };
     Matrix B = (Matrix){ .data = NULL, .rows = cfg->size, .cols = cfg->size };
     Matrix C = (Matrix){ .data = NULL, .rows = cfg->size, .cols = cfg->size };
@@ -282,107 +261,53 @@ ExperimentResult run_experiment(const ExperimentConfig* cfg, int rank) {
         random_uniform_matrix(&B, -1, 1);
     }
 
-    // Warm-up runs
-    for (int r = 0; r < cfg->warmup_repeats; r ++)
-        run_matmul(&A, &B, &C, cfg);
+    warmup_runs(&A, &B, &C, cfg);
 
-    double *times = NULL;
-    if (rank == 0) times = malloc(cfg->repeats * sizeof(double));
+    ExperimentResult results = create_results(cfg, rank);
     
+    double comm_time = 0.0;
+    double comp_time = 0.0;
     for (int r = 0; r < cfg->repeats; r++) {
-        if (rank == 0) fill_matrix(&C, 0.0f);    
+        if (rank == 0) {
+            fill_matrix(&C, 0.0f);
+            random_uniform_matrix(&A, -1, 1);
+            random_uniform_matrix(&B, -1, 1);
+        }
+        g_comm = 0.0;
+        g_comp = 0.0;
 
-#ifdef OPENMPI
-        MPI_Barrier(MPI_COMM_WORLD);
-#endif
+        BARRIER(MPI_COMM_WORLD);
+
         double time = -CLOCK_FN();
         run_matmul(&A, &B, &C, cfg);
 
-#ifdef OPENMPI
-        MPI_Barrier(MPI_COMM_WORLD);
-#endif
+        BARRIER(MPI_COMM_WORLD);
+
         time += CLOCK_FN();
 
-#ifndef OPENMPI
+#ifdef OPENMPI
+        MPI_Reduce(&g_comm, &comm_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+        MPI_Reduce(&g_comp, &comp_time, 1, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+#else
         time /= CLOCKS_PER_SEC;
 #endif
-        if (rank == 0) times[r] = time;
+
+        if (rank == 0 && cfg->verify) {
+            Matrix temp = create_matrix(cfg->size, cfg->size);
+
+            openblas_matmul(&A, &B, &temp);
+            results.correct = results.correct && is_equal_matrix(&C, &temp, 1e-5, 1e-3);
+
+            free_matrix(&temp);
+        }
+
+        log_timings(&results, r, time, comm_time, comp_time, rank);
     }
 
-    ExperimentResult result = {
-        .times = rank==0 ? times : NULL,
-        .repeats = cfg->repeats,
-        .correct = 1
-    };
 
-
-    if (rank == 0 && cfg->verify) {
-        Matrix temp = create_matrix(cfg->size, cfg->size);
-
-        openblas_matmul(&A, &B, &temp);
-        result.correct = is_equal_matrix(&C, &temp, 1e-5, 1e-3);
-
-        free_matrix(&temp);
-    }
 
     free_matrix(&A);
     free_matrix(&B);
     free_matrix(&C);
-    return result;
-}
-
-void print_results(const ExperimentConfig *cfg, const ExperimentResult *res) {
-    printf("===== Experiment Results =====\n");
-    printf("Matrix size:        %d x %d\n", cfg->size, cfg->size);
-    printf("Block size:         %d\n", cfg->block_size);
-    printf("Warmup rounds:      %d\n", cfg->warmup_repeats);
-    printf("Repetitions:        %d\n", cfg->repeats);
-    if (cfg->use_mpi)
-        printf("MPI grid:           %d x %d\n", cfg->proc_rows, cfg->proc_cols);
-    printf("OpenBLAS:           %s\n", cfg->use_openblas ? "ON" : "OFF");
-    printf("Implementation ID:  %d (%s)\n", cfg->implementation_id, impl_name(cfg->implementation_id));
-    printf("\n");
-
-    double mean = 0;
-    double std_dev = 0;
-
-    for (int i=0; i<res->repeats; i++) {
-        mean += res->times[i];
-    }
-    mean /= res->repeats;
-
-    if (cfg->repeats > 1) {
-        for (int i=0; i<res->repeats; i++) {
-            double time_res = res->times[i] - mean;
-            std_dev += time_res * time_res;
-        }
-        std_dev /= (res->repeats - 1);
-
-        std_dev = sqrt(std_dev);
-    }
-
-    printf("Mean runtime:       %.6f s\n", mean);
-    printf("Std. deviation:     %.6f s\n", std_dev);
-
-    if (cfg->verify)
-        printf("Verification:       %s%s%s\n",
-            res->correct ? "\033[1;32mPASSED" : "\033[1;31mFAILED",
-            "\033[0m", "");
-
-    printf("==============================\n");
-}
-
-void export_csv(char *path, const ExperimentConfig *cfg, const ExperimentResult *res) {
-    FILE *fp = fopen(path, "w");
-
-    fprintf(fp, "rep_id, size, block_size, BLAS, MPI, grid, time\n");
-    for (int i = 0; i < res->repeats; i++) {
-        fprintf(
-            fp, "%d, %d, %d, %d, %d, %dx%d, %.9g\n",
-            i, cfg->size, cfg->block_size, cfg->use_openblas, cfg->use_mpi,
-            cfg->proc_rows, cfg->proc_cols, res->times[i]
-        );
-    }
-
-    fclose(fp);
+    return results;
 }
